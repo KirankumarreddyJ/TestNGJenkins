aacf5ba9bab578af23146810a95ee5c5d3aba3af
Kirankumar Reddy Juturu <jkirankumarreddy9@gmail.com>
2023-03-07 19:21:21 +0530

    Parameterised

--- src/test/java/org/selenium/testng/BaseClass.java
+++ src/test/java/org/selenium/testng/BaseClass.java
@@ -4,23 +4,40 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public WebDriver driver;
-	
+
 	@BeforeSuite
-	public void beforeSuite() {
-		
-		 WebDriverManager.chromedriver().setup();
-		 driver = new ChromeDriver();
-		 driver.manage().window().maximize();
-		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	@Parameters("Browser")
+	public void beforeSuite(@Optional String browser) {
+		if(browser == null) {
+			browser = "Edge";
+		}else {
+			browser = browser;
+		}
+		if (browser.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
